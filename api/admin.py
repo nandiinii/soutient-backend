49c58651e2282612c82a69fa579007dbce22181f
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-04-28 19:11:07 +0530

    changes

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import (MetamaskUser,Election,Vote,CampaignDonation,LoanRequest,LoanInterested,Campaign, Review)
+from .models import (MetamaskUser,Election,Vote,CampaignDonation,LoanRequest,LoanInterested,Campaign, Review,InterestConfirmedAndContractDone)
 
 # Register your models here.
 admin.site.register(MetamaskUser)
@@ -10,4 +10,5 @@ admin.site.register(LoanInterested)
 admin.site.register(LoanRequest)
 admin.site.register(Campaign)
 admin.site.register(Review)
+admin.site.register(InterestConfirmedAndContractDone)
 # admin.site.register(Review)
